7c182db849d27c7ce591a8488b44b556a506b71d
yesolkim95 <godntr3707@gmail.com>
2022-03-19 23:25:49 +0900

    df

--- src/Exam01.java
+++ src/Exam01.java
@@ -5,7 +5,7 @@ public class Exam01
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
-		System.out.print("HEllo");
+		System.out.print("Hello world");	//수많은 실패끝에 첫 깃허브 성공!
 	}
 
 }
